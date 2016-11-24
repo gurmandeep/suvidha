abaab8fe364639cbf16234acf3e69653da328974
Gurmandeep <gsings.singh@gmail.com>
2016-11-24 11:07:55 +0530

    added child browser

--- www/js/script.js
+++ www/js/script.js
@@ -1,3 +1,8 @@
 jQuery(document).ready(function($) {
 	$('.sidebar').load("sidebar.html");
+
+	$("#openChild").click(function(event) {
+		window.plugins.ChildBrowser.showWebPage('http://www.google.com',
+                                        { showLocationBar: true });
+	});
 });
